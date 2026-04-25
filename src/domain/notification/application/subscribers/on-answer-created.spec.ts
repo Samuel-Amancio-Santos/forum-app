4f04490769075b7652d028829ed24ac4898c64ef
Samuel-Amancio-Santos <samuelamancio777@gmail.com>
2026-04-25 17:58:38 -0300

    chore:fix imports from test files, fix spyInstanc

--- src/domain/notification/application/subscribers/on-answer-created.spec.ts
+++ src/domain/notification/application/subscribers/on-answer-created.spec.ts
@@ -11,7 +11,7 @@ import {
 } from '../use-cases/send-notification'
 import { InMemoryNotificationsRepository } from '@test/repositories/in-memory-notifications-repository'
 import { makeQuestion } from '@test/factories/make-question'
-import { SpyInstance } from 'vitest'
+import { SpyInstance } from 'vitest' //TODO: SpyInstance was decrepeted
 import { waitFor } from '@test/utils/wait-for'
 import { InMemoryStudentsRepository } from '@test/repositories/in-memory-students-repository'
 import { InMemoryAttachmentsRepository } from '@test/repositories/in-memory-attachments-repository'
@@ -25,7 +25,7 @@ let inMemoryStudentsRepository: InMemoryStudentsRepository
 let inMemoryAttachmentsRepository: InMemoryAttachmentsRepository
 let sendNotificationUseCase: SendNotificationUseCase
 
-let sendNotificationExecuteSpy: SpyInstance<
+let sendNotificationExecuteSpy: SpyInstance< // (Espiar o envio de uma função, apos o evento de criação de question for dispachado)
   [SendNotificationUseCaseRequest],
   Promise<SendNotificationUseCaseResponse>
 >
--- src/domain/notification/application/use-cases/read-notification.spec.ts
+++ src/domain/notification/application/use-cases/read-notification.spec.ts
@@ -1,8 +1,8 @@
 import { ReadNotificationUseCase } from './read-notification'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { NotAllowedError } from '@/core/errors/errors/not-allowed-error'
-import { makeNotification } from '../../../../../@test/factories/make-notification'
-import { InMemoryNotificationsRepository } from '../../../../../@test/repositories/in-memory-notifications-repository'
+import { makeNotification } from '@/test/factories/make-notification'
+import { InMemoryNotificationsRepository } from '@/test/repositories/in-memory-notifications-repository'
 
 let inMemoryNotificationsRepository: InMemoryNotificationsRepository
 let sut: ReadNotificationUseCase
